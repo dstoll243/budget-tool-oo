15ac90b2e22e611717bf44f1d1776c1652a22145
github username <github email address>
2019-05-16 12:41:10 -0700

    Thanks for the lab, more pleasegit st

--- tools/console.rb
+++ tools/console.rb
@@ -6,5 +6,31 @@ def reload
   load 'config/environment.rb'
 end
 
+
+##customer objects
+laura = Customer.new("Laura")
+sarah = Customer.new("Sarah")
+kev = Customer.new("Kev")
+tyler = Customer.new("Tyler")
+spock = Customer.new("Spock")
+
+##location objects
+moon = Location.new("Moon")
+paris = Location.new("Paris")
+landfill = Location.new("Landfill")
+san_fransicso = Location.new("San Fransisco")
+
+##transaction objects
+t1 = Transaction.new(laura, moon, 100)
+t2 = Transaction.new(kev, moon, 200)
+t3 = Transaction.new(kev, paris, 150)
+t4 = Transaction.new(sarah, landfill, 10)
+t5 = Transaction.new(tyler, san_fransicso, 1000)
+t6 = Transaction.new(spock, landfill, 1)
+t7 = Transaction.new(laura, moon, 5)
+
+
+
+
 binding.pry
 0
