d1d7d6825e9664e3732590cbd18bc40c2de36f11
jjsmokovich <JesseSmokovich@yahoo.com>
2019-05-16 16:17:13 -0700

    done

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,37 @@
- # TRANSACTION TRACKER OO
+class Customer
+  attr_accessor :name
+  
+  @@all = []
 
-# Write out your customer class and methods
+  def initialize(name) 
+    @name = name 
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def get_cust
+    Transaction.all.select do |trans|
+      trans.customer == self
+    end
+  end
+
+  def locations 
+    get_cust.map {|cust| cust.location}
+  end
+
+  def transactions 
+    get_cust #redundant
+  end
+
+  def get_amount
+    get_cust.map {|cust| cust.amount}
+  end
+
+  def total_spent
+    get_amount.inject {|sum, n| sum + n}
+  end
+
+end
--- lib/location.rb
+++ lib/location.rb
@@ -1,3 +1,29 @@
- # TRANSACTION TRACKER OO
+class Location 
+  attr_accessor :name
+  
+  @@all = []
 
-# Write out your Location class and methods
+  def initialize(name) 
+    @name = name
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def transactions
+    Transaction.all.select do |local| 
+      local.location == self
+    end
+  end
+
+  def customers
+    transactions.map {|local| local.customer}
+  end
+
+  def unique_customers 
+    customers.uniq
+  end
+
+end
