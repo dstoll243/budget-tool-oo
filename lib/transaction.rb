a6ed711a93cc96a3f960aa33f831e0afe7ae258b
dahby <dstoll243@gmail.com>
2019-05-15 20:03:51 -0700

    init commit

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,1 +1,21 @@
 # Write out your transaction class and methods
+
+class Transaction
+  attr_accessor :user, :account, :location, :amount, :category, :confirmed
+
+  @@all = []
+
+  def initialize(user, account, amount, location=nil)
+    @user = user
+    @account = account
+    @amount = amount
+    @location = location
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
--- lib/user.rb
+++ lib/user.rb
@@ -1,1 +1,21 @@
 # Write out your user class and methods
+
+class User
+  attr_reader :username, :password
+
+  @@all = []
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def confirm_user
+    "Hello, #{@username}"
+  end
+end
